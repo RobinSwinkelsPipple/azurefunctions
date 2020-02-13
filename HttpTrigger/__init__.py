6d73cf501478750ae224e5a3bea9aae5ce4c9bff
RobinSwinkelsPipple <robin@pipple.nl>
2020-02-13 11:31:58 +0100

    rewritten code

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,24 +1,30 @@
 import logging
-
+import random
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Get params and request body
     name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
+    resp = func.HttpResponse
+    
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        if not name:
             name = req_body.get('name')
 
+    # Compute score 
+    score = random.randint(0,100)
+
+    # Prepare response
     if name:
-        return func.HttpResponse(f"Hello {name}!")
+        response = resp(f"Hello {name}! \n\nYour credit score is: {score}")
     else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+        response = resp("Please pass a name on the query string or in the request body", status_code=400)
+
+    return response

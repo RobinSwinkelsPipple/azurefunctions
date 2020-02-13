27260cccd76b94dd767bfd6a13d88a9f0cc10c6e
RobinSwinkelsPipple <robin@pipple.nl>
2020-02-13 13:26:05 +0100

    rewritten code

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -3,28 +3,40 @@ import random
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    # Get params and request body
-    name = req.params.get('name')
-    resp = func.HttpResponse
-    
-    try:
-        req_body = req.get_json()
-    except ValueError:
-        pass
-    else:
-        if not name:
-            name = req_body.get('name')
-
-    # Compute score 
-    score = random.randint(0,100)
-
-    # Prepare response
+def prepare_response(name, resp, score):
     if name:
         response = resp(f"Hello {name}! \n\nYour credit score is: {score}")
     else:
         response = resp("Please pass a name on the query string or in the request body", status_code=400)
+    return response
+
+
+def compute_credit_score():
+    return random.randint(0,100)
+
+
+def parse_request(request):     
+    name = request.params.get('name')
+        
+    try:
+        request_body = request.get_json()
+    except ValueError:
+        pass
+    else:
+        if not name:
+            name = request_body.get('name')
+
+    return name
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    resp = func.HttpResponse
+
+    name = parse_request(req)
+    
+    score = compute_credit_score()
+
+    response = prepare_response(name, resp, score)
 
     return response

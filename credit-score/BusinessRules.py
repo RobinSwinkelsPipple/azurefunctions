2bf8ac2375ff986154e542f1e99e512220aa2830
RobinSwinkelsPipple <robin@pipple.nl>
2020-02-20 15:27:58 +0100

    small changes to BR

--- credit-score/BusinessRules.py
+++ credit-score/BusinessRules.py
@@ -5,19 +5,19 @@ class BusinessRules:
     """Base class for BusinessRules"""
 
     def _decision_function(self, context):
-        # score = 0
+        returncode = 0
         phone_nr = context.get('phone_nr')
         size = context.get('size')
         crop = context.get('crop') 
         weather = context.get('weather') 
         location = context.get('location')
-        if crop != 'tomatoes' or weather != 'sunny' or location != 'Nairobi':
-            return -1
-        if not phone_nr.startswith('0254'):
-            return -1
-        if size > 30:
-            return -1
-        return random.randint(0, 100)
+        if crop != 'tomatoes' or weather != 'sunny' or location != 'Nairobi' or not phone_nr.startswith('0254') or size > 30:
+            returncode = -1
+        else:
+            returncode = random.randint(0,100)
 
+        return returncode
+
+        
     def predict(self, context):
         return self._decision_function(context)
